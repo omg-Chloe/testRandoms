4626808fcaf33d1dc70ffc7133b8bcb446a66015
Chloe Odessa Cutler <38904022+omg-Chloe@users.noreply.github.com>
2023-04-16 22:00:11 -0400

    float fix

--- aveDepDelayByMonthReducer.py
+++ aveDepDelayByMonthReducer.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 import sys
 last_key = None
-count = 0
-sum = 1
+count = 0.0
+sum = 1.0
 # keys come grouped together so we need to keep track of state a little bit  thus when the key changes, 
 # we need to reset our counter, and write out the count we've accumulated
 for line in sys.stdin:
@@ -15,17 +15,17 @@ for line in sys.stdin:
    # if this is the first iteration
    if not last_key:
        last_key = key
-       count = 1
+       count = 1.0
        sum = value
     # if they're the same, log it
    elif key == last_key:
-        count = count + 1
+        count = count + 1.0
         sum = sum + value
    else: 
         result = [last_key, sum/count]
         print("\t".join(str(v) for v in result))
         last_key = key
-        count = 1
+        count = 1.0
         sum = value
 # this is to catch the final value that we output
 print("\t".join(str(v) for v in [last_key, sum/count]))
