d42d6ebf0d2c6019761298ec9f20973b4119727b
Chloe Odessa Cutler <38904022+omg-Chloe@users.noreply.github.com>
2023-04-25 20:35:47 -0400

    Null handle

--- TaxiMapper.py
+++ TaxiMapper.py
@@ -11,5 +11,9 @@ for line in sys.stdin:
     line = line.strip()
     unpacked = line.split(",")
     Year, Month, DayofMonth, DayOfWeek, DepTime, CRSDepTime, ArrTime, CRSArrTime, UniqueCarrier, FlightNum, TailNum, ActualElapsedTime, CRSElapsedTime, AirTime, ArrDelay, DepDelay, Origin, Dest, Distance, TaxiIn,TaxiOut, Cancelled, CancellationCode, Diverted, CarrierDelay, WeatherDelay, NASDelay, SecurityDelay, LateAircraftDelay = line.split(",")
+    if TaxiIn=="NA":
+        TaxiIn=0
+    if TaxiOut=="NA":
+        TaxiOut=0
     results = ["-".join([UniqueCarrier,Origin]),str(int(TaxiIn) + int(TaxiOut))]]
     print("\t".join([results]))
