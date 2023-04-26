537b404b402f8ca49d6ea1ece88a323680db7cd3
Chloe Odessa Cutler <38904022+omg-Chloe@users.noreply.github.com>
2023-04-25 20:40:47 -0400

    edit

--- TaxiReducerEXAMPLE.py
+++ TaxiReducerEXAMPLE.py
@@ -2,7 +2,7 @@
 import sys
 last_key = None
 count = 0
-sum=1
+sum = 1
 min = 0
 max = 0
 # keys come grouped together  so we need to keep track of state a little bit thus when the key changes , we need to reset
@@ -13,27 +13,23 @@ for line in sys.stdin:
    key, value = line.split("\t")
 
    # we have to be able to deal with missing values
-   if value =="NA":
+   if value == "NA":
        continue
-
    value = float(value)
-
    # if this is the first iteration
    if not last_key:
        last_key = key
        count = 1 
-       min=value
-       max=value
-       sum=value
-
-
+       min = value
+       max = value
+       sum = value
     # if they're the same, log it
    elif key == last_key:
         count = count + 1 
         if value < min:
-            min =value  
+            min = value  
         if value > max:
-            max=value
+            max = value
         sum = sum + value
 
    else: 
@@ -41,7 +37,7 @@ for line in sys.stdin:
         print("\t".join(str(v) for v in result))
         last_key = key
         count = 1
-        sum=value
+        sum = value
         min = value
         max = value
             
