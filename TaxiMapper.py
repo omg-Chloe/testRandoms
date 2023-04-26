d73df80c24886574c19eb4178eab27738681c824
Chloe Odessa Cutler <38904022+omg-Chloe@users.noreply.github.com>
2023-04-25 20:58:08 -0400

    Create TaxiMapper.py

---
+++ TaxiMapper.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import sys
+
+# -- Airline Data
+# Year, Month, DayofMonth, DayOfWeek, DepTime, CRSDepTime, ArrTime, CRSArrTime, UniqueCarrier, FlightNum,
+# TailNum, ActualElapsedTime, CRSElapsedTime, AirTime, ArrDelay, DepDelay, Origin, Dest, Distance, TaxiIn,
+# TaxiOut, Cancelled, CancellationCode, Diverted, CarrierDelay, WeatherDelay, NASDelay, SecurityDelay, LateAircraftDelay
+
+for line in sys.stdin:
+    line = line.strip()
+    unpacked = line.split(",")
+
+
+    Year, Month, DayofMonth, DayOfWeek, DepTime, CRSDepTime, ArrTime, CRSArrTime, UniqueCarrier, FlightNum, TailNum, ActualElapsedTime, CRSElapsedTime, AirTime, ArrDelay, DepDelay, Origin, Dest, Distance, TaxiIn,TaxiOut, Cancelled, CancellationCode, Diverted, CarrierDelay, WeatherDelay, NASDelay, SecurityDelay, LateAircraftDelay = line.split(",")
+    if TaxiIn=="NA":
+        TaxiIn=0
+    if TaxiOut=="NA":
+        TaxiOut=0
+    results = ["-".join([UniqueCarrier, Origin]), str(int(TaxiIn) + int(TaxiOut))]
+    print("\t".join(results))
