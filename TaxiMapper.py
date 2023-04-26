5095c6b93480eb8eda6d7cf9befd594768bcd95d
Chloe Odessa Cutler <38904022+omg-Chloe@users.noreply.github.com>
2023-04-25 21:02:45 -0400

    Update TaxiMapper.py

--- TaxiMapper.py
+++ TaxiMapper.py
@@ -13,9 +13,9 @@ for line in sys.stdin:
 
 
     Year, Month, DayofMonth, DayOfWeek, DepTime, CRSDepTime, ArrTime, CRSArrTime, UniqueCarrier, FlightNum, TailNum, ActualElapsedTime, CRSElapsedTime, AirTime, ArrDelay, DepDelay, Origin, Dest, Distance, TaxiIn,TaxiOut, Cancelled, CancellationCode, Diverted, CarrierDelay, WeatherDelay, NASDelay, SecurityDelay, LateAircraftDelay = line.split(",")
-    if TaxiIn=="NA":
-        TaxiIn=0
-    if TaxiOut=="NA":
-        TaxiOut=0
+    if TaxiIn == "NA":
+        TaxiIn = 0
+    if TaxiOut == "NA":
+        TaxiOut = 0
     results = ["-".join([UniqueCarrier, Origin]), str(int(TaxiIn) + int(TaxiOut))]
     print("\t".join(results))
