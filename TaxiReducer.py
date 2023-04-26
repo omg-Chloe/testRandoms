7b3cf24076bc44e0fcf935fe8f0666a64f580115
Chloe Odessa Cutler <38904022+omg-Chloe@users.noreply.github.com>
2023-04-25 20:28:24 -0400

    yikes

--- TaxiReducer.py
+++ TaxiReducer.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 import sys
 last_key = None
-min_val = float("inf")
-max_val = float("-inf")
+min_val = 0.0
+max_val = 0.0
 sum = 0.0
 count = 0
 # keys come grouped together so we need to keep track of state a little bit  thus when the key changes, 
@@ -17,15 +17,11 @@ for line in sys.stdin:
    # if this is the first iteration
    if not last_key:
        last_key = key
-       if float(value) < min_val:
-            min_val = float(value)
-        if float(value) > max_val:
-            max_val = float(value)
         # Update the sum and count variables
         sum = float(value)
+        min_val = value
+        max_val = value
         count = 1
-        # Calculate the average value
-        avg = sum / count
     # if they're the same, log it
    elif key == last_key:
         if float(value) < min_val:
@@ -35,8 +31,6 @@ for line in sys.stdin:
         # Update the sum and count variables
         sum = sum + float(value)
         count = count + 1
-        # Calculate the average value
-        avg = sum / count
    else: 
         result = [last_key, sum/count,min,max]
         print("\t".join(str(v) for v in result))
@@ -44,4 +38,4 @@ for line in sys.stdin:
         count = 1
         sum = value
 # this is to catch the final value that we output
-print("\t".join(str(v) for v in [last_key, avg,min_val,max_val]))
+print("\t".join(str(v) for v in [last_key, sum / count, min_val,max_val]))
